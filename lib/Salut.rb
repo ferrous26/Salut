57e0f6b57febc926e49cac80ea8c4f9e2a60c1f4
Mark Rada <mrada@marketcircle.com>
2011-01-16 21:11:40 -0500

    Load framework from the top level file

--- lib/Salut.rb
+++ lib/Salut.rb
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
-require 'Salut/Service'
-require 'Salut/Browser'
+framework 'Foundation'
+
+require   'Salut/Service'
+require   'Salut/Browser'
 
 # A class to help with advertising services using Bonjour and finding
 # other services that are being advertised using Bonjour.
--- lib/Salut/Service.rb
+++ lib/Salut/Service.rb
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-framework 'Foundation'
 
 module Salut
 
