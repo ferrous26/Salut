678aa2be48d7bd3b09eb9f98b5c60b2882a6ce99
Mark Rada <mrada@marketcircle.com>
2011-01-16 20:42:12 -0500

    Add some tests for Service#advertising?
    Right now I have one failing test, but that is due to difficulties
    in testing, not because of a bug.

---
+++ spec/Service_spec.rb
@@ -0,0 +1,49 @@
+require './spec_helper'
+
+describe Salut::Service do
+
+
+  describe '#advertising?' do
+    before do
+      @service = Salut::Service.new({
+        port:3000,
+        instance_name:'Test',
+        service_type:'_http._tcp.'
+      })
+    end
+
+    it 'should be initialized to false' do
+      @service.advertising?.should.be.equal false
+    end
+
+    it 'should be false when I start advertising (until #netServiceDidPublish is called)' do
+      @service.start_advertising
+      @service.advertising?.should.be.equal false
+    end
+
+    it 'should be false after I stop advertising' do
+      @service.start_advertising
+      NSRunLoop.currentRunLoop.runUntilDate Time.now + 2
+      @service.stop_advertising
+      NSRunLoop.currentRunLoop.runUntilDate Time.now + 2
+      @service.advertising?.should.be.equal false
+    end
+
+    it 'should be false if advertising fails' do
+      @other_service = @service.dup
+      @service.start_advertising
+      NSRunLoop.currentRunLoop.runUntilDate Time.now + 2
+      @other_service.start_advertising
+      NSRunLoop.currentRunLoop.runUntilDate Time.now + 2
+      @other_service.advertising?.should.be.equal false
+    end
+
+    it 'should be true when advertising is successful' do
+      @service.start_advertising
+      NSRunLoop.currentRunLoop.runUntilDate Time.now + 2
+      @service.advertising?.should.be.equal true
+    end
+  end
+
+
+end
