a8f39835e0f28e7e00a211a1bff12981e49ab54b
Mark Rada <mrada@marketcircle.com>
2011-01-16 20:44:29 -0500

    No need to require the framework in the top level
    The individual classes will do it themselves.

--- lib/Salut.rb
+++ lib/Salut.rb
@@ -1,8 +1,6 @@
 # -*- coding: utf-8 -*-
-framework 'Foundation'
-
-require   'Salut/Service'
-require   'Salut/Browser'
+require 'Salut/Service'
+require 'Salut/Browser'
 
 # A class to help with advertising services using Bonjour and finding
 # other services that are being advertised using Bonjour.
